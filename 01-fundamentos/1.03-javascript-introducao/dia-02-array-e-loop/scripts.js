4a11c97d2fbaa739e97a5aad5ab0151790992c58
Clinton Brito <clintoncsb@gmail.com>
2023-01-27 13:59:31 -0300

    Resolvendo exercícios do 2o dia da Seção 3 - Introdução à JavaScript v2

--- 01-fundamentos/1.03-javascript-introducao/dia-02-array-e-loop/scripts.js
+++ 01-fundamentos/1.03-javascript-introducao/dia-02-array-e-loop/scripts.js
@@ -16,21 +16,54 @@
 
 // Calcule e imprima a média aritmética dos valores contidos no array;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let somatorio = 0;
-let media = 0;
-for (index = 0; index < numbers.length; index += 1) {
-    somatorio += numbers[index];
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let somatorio = 0;
+// let media = 0;
+// for (index = 0; index < numbers.length; index += 1) {
+//     somatorio += numbers[index];
+// }
 
-media = somatorio / (numbers.length);
-console.log("O somatório dos números é:", somatorio);
-console.log("A média aritmética dos números é:", media);
+// media = somatorio / (numbers.length);
+// console.log("O somatório dos números é:", somatorio);
+// console.log("A média aritmética dos números é:", media);
 
 // Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
-if (media > 20) {
-    console.log("Valor da Média Aritmética é maior que 20 :)")
-} else {
-    console.log("Valor da Média Aritmética é menor ou igual a 20 :)")
-}
+// if (media > 20) {
+//     console.log("Valor da Média Aritmética é maior que 20 :)")
+// } else {
+//     console.log("Valor da Média Aritmética é menor ou igual a 20 :)")
+// }
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+// let largestNumber = numbers [0];
+
+// for (index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > largestNumber)
+//     largestNumber = numbers[index];
+// }
+
+// console.log(largestNumber)
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let oddNumbers = 0;
+
+// for (index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//         oddNumbers += 1;
+//     }
+// }
+
+// if (oddNumbers === 0) {
+//     console.log("Nenhum valor ímpar encontrado :(")
+// }
+
+// console.log(oddNumbers)
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
